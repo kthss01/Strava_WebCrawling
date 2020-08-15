6f28c127a32ac22f53be398451c370f61f8cc117
kthss01 <kthss01@naver.com>
2020-08-15 23:40:33 +0900

    json-simple을 통한 json parse 스트라바 데이터에서 (로그인 하지 않은 정보에서) 필요한 데이터 선택 및 파악하여 정리

---
+++ JsonParse_Practice/src/JsonSimpleTest.java
@@ -0,0 +1,114 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class JsonSimpleTest {
+
+	public static void printJsonObject(Object obj) {
+		JSONObject jsonObj = (JSONObject)obj; 
+		jsonObj.forEach((key, value) -> System.out.println(key + " " + value));
+	}
+
+	public static void printJsonArray(Object arr) {
+		JSONArray jsonArr = (JSONArray) arr;
+		for (int i = 0; i < jsonArr.size(); i++) {
+			printJsonObject((JSONObject) jsonArr.get(i));
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) throws IOException, ParseException {
+		Document doc = Jsoup.connect("https://www.strava.com/athletes/51315032").get();
+		Elements contents = doc.select("div");
+		String json = contents.attr("data-react-props");
+//		System.out.println(json);
+//		System.out.println();
+
+		JSONParser parser = new JSONParser();
+
+		JSONObject obj = null;
+		obj = (JSONObject) parser.parse(json);
+//		System.out.println(obj);
+
+//		obj.forEach((key, value) -> System.out.println(key + " " + value));
+//		printJsonObject(obj);
+		/*
+		 * athlete - 스트라바 선수 정보 
+		 * stats - 선수 스텟 
+		 * recentActivities - 최근 활동 내역
+		 */
+
+//		System.out.println(obj.get("athlete"));
+//		((JSONObject) obj.get("athlete")).forEach((key, value) -> System.out.println(key + " " + value));
+//		printJsonObject(obj.get("athlete"));
+
+		/*
+		 * avatarUrl 
+		 * name 
+		 * primarySport 
+		 * location 
+		 * followerCount 
+		 * follwoingCount
+		 */
+
+//		System.out.println(obj.get("stats"));
+//		printJsonObject(obj.get("stats"));
+		/*
+		 * chartData - JSONArray, 달별 기록
+		 * monthlyTime
+		 * monthlyDistance
+		 */
+		
+//		printJsonArray((JSONArray)((JSONObject)obj.get("stats")).get("chartData"));
+		/*
+		 * hours
+		 * month
+		 * elev_gain
+		 * miles
+		 */
+
+//		System.out.println(obj.get("recentActivities")); // JSONArray
+//		printJsonArray(obj.get("recentActivities"));
+		/*
+		 * elevation
+		 * images - JSONArray 
+		 * distance
+		 * hasGps
+		 * startDateLocal
+		 * isManual
+		 * name
+		 * detailedType
+		 * movingTime
+		 * id
+		 * type 
+		 */
+		
+		printJsonArray(((JSONObject)((JSONArray)obj.get("recentActivities")).get(0)).get("images"));
+		/*
+		 * defaultSrc 
+		 * squareSrc
+		 * caption
+		 * type
+		 * primary
+		 */
+		
+		JSONArray arr = null;
+		arr = (JSONArray) obj.get("recentActivities");
+
+		for (int i = 0; i < arr.size(); i++) {
+			obj = (JSONObject) arr.get(i);
+//			System.out.println(obj);
+		}
+
+//		arr = (JSONArray)parser.parse(json);
+//		System.out.println(arr);
+
+	}
+}
--- WebCrwaling_Practice/src/JsoupTest.java
+++ WebCrwaling_Practice/src/JsoupTest.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -9,12 +10,17 @@ public class JsoupTest {
 	public static void main(String[] args) {
 		try {
 			// 웹에서 내용을 가져온다.
-			Document doc = Jsoup.connect("https://jobc.tistory.com/").get();
-			System.out.println(doc);
+//			Document doc = Jsoup.connect("https://jobc.tistory.com/").get();
+			Document doc = Jsoup.connect("https://www.strava.com/athletes/51315032").get();
+//			System.out.println(doc);
 			
 			// 내용 중에서 원하는 부분을 가져온다.
-			Elements contents = doc.select("h1");
+//			Elements contents = doc.select("h1");
+			Elements contents = doc.select("div");
 //			System.out.println(contents);
+//			System.out.println(contents.attr("data-react-props"));
+			
+			String str = contents.attr("data-react-props");			
 			
 			// 원하는 부분은 Elements 형태로 되어 있으므로 이를 String 형태로 바꾸어 준다.
 			String text = contents.text();
